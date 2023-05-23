b565400aca95b4e9182bbb3e3c731162b3428b24
Kacper Śleziak <kacpersleziak00@gmail.com>
2023-05-23 18:28:27 +0200

    docker

---
+++ db/graphdb-data/work/workbench/settings.js
@@ -0,0 +1,22 @@
+{
+  "users" : {
+    "admin" : {
+      "username" : "admin",
+      "password" : "{bcrypt}$2a$10$MmPk/Leb8WnSw23g88BsUew3BqPTIGwGcGDijyzsl.qYPgYYtFc2G",
+      "grantedAuthorities" : [ "ROLE_ADMIN" ],
+      "appSettings" : {
+        "DEFAULT_INFERENCE" : true,
+        "DEFAULT_VIS_GRAPH_SCHEMA" : true,
+        "DEFAULT_SAMEAS" : true,
+        "IGNORE_SHARED_QUERIES" : false,
+        "EXECUTE_COUNT" : true
+      },
+      "dateCreated" : 1684858969525
+    }
+  },
+  "import.server" : { },
+  "import.local" : { },
+  "properties" : {
+    "current.location" : ""
+  }
+}
